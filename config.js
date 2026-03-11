f2f4c33e3c70dae9990bc94282c919e654c0c8af
kksdave <35850027+kksdave@users.noreply.github.com>
2026-03-11 15:24:59 -0400

    Update config.js

--- config.js
+++ config.js
@@ -5,5 +5,5 @@
 // Supabase → Project Settings → API
 // =============================================
 
-const SUPABASE_URL = 'YOUR_SUPABASE_URL';
-const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY';
+const SUPABASE_URL = 'https://vraioksaisexrcczgozl.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZyYWlva3NhaXNleHJjY3pnb3psIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwNzM3MDcsImV4cCI6MjA4ODY0OTcwN30.PyqQnFZmpwFP1bBRye2gkI3PlNBiDH6yXk2f6VXqiMs';
